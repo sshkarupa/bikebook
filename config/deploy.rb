e45a529313ca3c092a32cb93b823e23c06d93d10
Pavel Shcheglov <p.scheglov@gmail.com>
2015-08-14 17:57:05 +0300

    cap +++

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,5 +8,23 @@ set :keep_releases, 5
 set :linked_files, %w{config/application.yml config/secrets.yml config/unicorn.rb config/database.yml}
 set :linked_dirs, %w{log tmp vendor/bundle public/assets public/system}
 
+namespace :figaro do
+  desc "SCP transfer figaro configuration to the shared folder"
+  task :setup do
+    on roles(:app) do
+      upload! "config/application.yml", "#{shared_path}/config/application.yml", via: :scp
+    end
+  end
+
+  desc "Symlink application.yml to the release path"
+  task :symlink do
+    on roles(:app) do
+      execute "ln -sf #{shared_path}/application.yml #{current_path}/config/application.yml"
+    end
+  end
+end
+
+after "deploy:started", "figaro:setup"
+after "deploy:symlink:release", "figaro:symlink"
 after 'deploy:publishing', 'deploy:restart'
 after 'deploy:restart', 'unicorn:duplicate'
