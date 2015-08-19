f94e0e389cd75a6509b9e703e5d62b303c282a85
Pavel Shcheglov <p.scheglov@gmail.com>
2015-08-19 12:42:53 +0300

    refresh home style

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,7 +2,7 @@ class Picture < ActiveRecord::Base
   belongs_to :bike
 
   has_attached_file :img,
-                    :styles => { :medium_l => "600x450#", :medium => "300x300#", :medium_s => "200x200#", :thumb => "100x100#" },
+                    :styles => { :medium_l => "600x450#", :medium => "235x235#", :medium_s => "200x200#", :thumb => "100x100#" },
                     :convert_options => {:medium => "-quality 75 -strip" }
 
   validates_attachment_presence :img
