c6b68bd2aaa476f0b0c132b3295f9961d49cc75e
Pavel Shcheglov <p.scheglov@gmail.com>
2015-08-21 14:54:45 +0300

    deploy fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :use_sudo, false
 set :rvm_type, :user
 set :keep_releases, 5
 
-set :linked_files, %w{config/application.yml config/secrets.yml config/database.yml}
+set :linked_files, %w{config/application.yml config/secrets.yml config/database.yml config/mainsms.yml}
 set :linked_dirs, %w{log tmp vendor/bundle public/assets public/system}
 
 namespace :figaro do
