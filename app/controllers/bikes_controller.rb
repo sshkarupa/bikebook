1bd58f653205695f8fd8001d959f777565dcd11b
Pavel Shcheglov <p.scheglov@gmail.com>
2015-08-27 16:02:00 +0300

    show only approved bikes

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,7 +1,7 @@
 class BikesController < ApplicationController
 
   def show
-    @bike = Bike.find(params[:id])
+    @bike = Bike.approved.find(params[:id])
     @user = User.find(@bike.user_id)
     @department = Department.find(@bike.department)
     @city = @department.cities.find_by(id: @bike.city)
