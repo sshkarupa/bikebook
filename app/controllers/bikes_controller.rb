51bc3964345bf542e102ae62cb3126a2d24e8e02
Pavel Shcheglov <p.scheglov@gmail.com>
2015-08-28 13:31:48 +0300

    user can see own bikes if they is not approved

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,7 +1,16 @@
 class BikesController < ApplicationController
 
   def show
-    @bike = Bike.approved.find(params[:id])
+    if user_signed_in?
+      if current_user.role == 'user'
+        @bike = current_user.bikes.find(params[:id])
+      elsif current_user.role == 'admin'
+        @bike = Bike.find(params[:id])
+      end
+    else
+      @bike = Bike.approved.find(params[:id])
+    end
+
     @user = User.find(@bike.user_id)
     @department = Department.find(@bike.department)
     @city = @department.cities.find_by(id: @bike.city)
@@ -10,7 +19,6 @@ class BikesController < ApplicationController
       format.html
       format.json { render json: @user }
     end
-
   end
 
 end
